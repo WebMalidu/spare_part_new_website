bd25eb0b9e5d80cdeb51129644e00686c7821144
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2023-08-29 15:17:41 +0530

    WhyChooseUS
    create ui

---
+++ frontend/chooseUsandReview.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- js -->
+    <script defer src="js/bootstrap.bundle.js"></script>
+    <script defer src="js/script.js"></script>
+
+</head>
+
+<body>
+    <section class=" WCU-s1 bg-primary w-100">
+        <div class="container">
+            <div class="w-100 text-light fw-bold fs-4 d-flex justify-content-center">
+                <p>Why Choose Us</p>
+            </div>
+            <div class="w-100 d-flex justify-content-center p-3 gap-5">
+                <div class="WCU-s1-sBox d-flex flex-column justify-content-center align-items-center rounded-2 gap-4 text-center">
+                    <div class="w-100 ">
+                        <img src="resources/image/home/test.png" alt="" class="WCU-s1-image">
+                    </div>
+                    <div class="w-100 WCU-s1-box-para text-primary fw-bold">Accuracy</div>
+                </div>
+                <div class="WCU-s1-sBox d-flex flex-column justify-content-center align-items-center rounded-2 gap-4 text-center">
+                    <div class="w-100 ">
+                        <img src="resources/image/home/test.png" alt="" class="WCU-s1-image">
+                    </div>
+                    <div class="w-100 WCU-s1-box-para text-primary fw-bold">Accuracy</div>
+                </div>
+                <div class="WCU-s1-sBox d-flex flex-column justify-content-center align-items-center rounded-2 gap-4 text-center">
+                    <div class="w-100  ">
+                        <img src="resources/image/home/test.png" alt="" class="WCU-s1-image">
+                    </div>
+                    <div class="w-100 WCU-s1-box-para text-primary fw-bold">Accuracy</div>
+                </div>
+        
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
