2612bbff203ada787cc19591a1c851eb8268a882
kavindu202 <kavindu.sasanka962@gmail.com>
2023-08-30 20:28:39 +0530

    update category

---
+++ frontend/interfaces/offerSection.php
@@ -0,0 +1,44 @@
+<section>
+    <div class="row">
+        <div class="col-12">
+            <h3 class="mx-5">Offer Section</h3>
+            <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="resources/image/offer.png" alt="">
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="resources/image/offer.png" alt="">
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="resources/image/offer.png" alt="">
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="resources/image/offer.png" alt="">
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="resources/image/offer.png" alt="">
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div>
+                            <img src="resources/image/offer.png" alt="">
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</section>
