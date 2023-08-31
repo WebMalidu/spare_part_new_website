f408cd6b7f3ae28f03f109fc82b46837360c954c
MADUSHADEV <madusha.dev001@gmail.com>
2023-08-31 20:50:46 +0530

    upload my folder on my section

---
+++ frontend/temp/madusha/script.js
@@ -0,0 +1,8 @@
+// swiper animation
+var swiper = new Swiper(".mySwiperld", {
+     direction: "vertical",
+     pagination: {
+       el: ".swiper-pagination",
+       clickable: true,
+     },
+   });
