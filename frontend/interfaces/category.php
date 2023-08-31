64e3e98f922e23cccd1d9468e468685bc7201769
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2023-08-31 22:49:45 +0530

    catergory.php
    add padding

--- frontend/interfaces/category.php
+++ frontend/interfaces/category.php
@@ -37,7 +37,7 @@
 
             </div>
 
-            <div class="d-flex justify-content-center mt-5">
+            <div class="d-flex justify-content-center mt-5 p-3">
                 <button class="btn btn-outline-primary fw-semibold" onclick='increaseCategory(<?php echo $num_row ?>)'>Learn More</button>
             </div>
 
