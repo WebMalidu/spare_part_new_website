f3f6039837403722019df82bf4769f90f4503fa9
kaviska <126580314+kaviska@users.noreply.github.com>
2023-09-01 10:30:29 -0700

    e

--- frontend/interfaces/category.php
+++ frontend/interfaces/category.php
@@ -1,16 +1,16 @@
 <section class="py-5">
     <div class="container">
         <h3 class=" alg-text-blue mt-3 mb-4 fs-2 fw-bold">Search By Category</h3>
-        <div class="row d-flex justify-content-center">
-            <div class="col-8 d-flex justi align-content-cente flex-wrap gap-5 gap-lg-4" id="category">
+        <div class="row d-flex ">
+            <div class="col-12 d-flex justi align-content-cente flex-wrap gap-5 gap-lg-4" id="category">
 
                 <?php
                 $num_row = 12;
 
                 if ($num_row >= 5) {
-                    for ($x = 0; $x < 5; $x++) {
+                    for ($x = 0; $x < 4; $x++) {
                 ?>
-                        <div class="col-5 col-md-3 col-lg-2 alg-bg-category mb-3 rounded mt-3">
+                        <div class="col-5 col-md-3 col-lg-2 alg-bg-category mb-3 rounded mt-3 mx-4">
 
                             <div class="d-flex flex-column align-items-center">
                                 <img src="resources/image/car.jpg" alt="" class="alg-category-img mt-4 mb-3">
