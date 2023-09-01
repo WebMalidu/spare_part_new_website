3c997576ffc0eec5a5a0e88f2aa1eef7ef6dd537
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2023-09-01 20:06:18 +0530

    s
    add css

--- frontend/footer.php
+++ frontend/footer.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Slider</title>
     <!-- CSS -->
+    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.0.9/dist/css/splide.min.css">
 
