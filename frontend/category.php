ee67004ae0b6d8ca8d66d82872aefa670e16e86a
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2023-10-12 11:48:50 +0530

    bug

--- frontend/category.php
+++ frontend/category.php
@@ -49,7 +49,7 @@
                             <input class="form-check-input" type="checkbox">
                             <input class="form-check-input" type="checkbox">
                         </div>
-                        <div class="d-flex flex-column  ">
+                        <div class="d-flex flex-column  gap-2">
                             <span class="alg-text-h3 alg-text-light">Aftermarket (30)</span>
                             <span class="alg-text-h3 alg-text-light">OEM (6)</span>
                         </div>
