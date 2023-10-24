1bf283c082810ee0ae6c8f262b3487db3eac226d
kavindu202 <kavindu.sasanka962@gmail.com>
2023-10-24 05:56:27 +0530

    dd

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -24,13 +24,17 @@ function loadCategory() {
             const categoryContainer = document.getElementById("categoryContainer");
 
             if (data.status == "success") {
-                categoryContainer.innerHTML = ` <div class="col-6 col-md-4 col-lg-2 alg-bg-categor alg-shadow mb-1 rounded mt-3 mx-4 px-3 alg-card-hover">
-        <a href="category.php" class="text-decoration-none">
-            <div class="d-flex flex-column align-items-center">
-                <img src="resources/image/car.jpg" alt="" class="alg-category-img mt-4 mb-4 img-fluid">
-                <span class="mt-1 p-3 fw-bold text-whit pb-5 alg-text-h3">Air Condition</span>
-            </div>
-    </div></a>`
+                categoryContainer.innerHTML = "";
+                data.results.forEach(element => {
+                    categoryContainer.innerHTML += ` <div class="col-6 col-md-4 col-lg-2 alg-bg-categor alg-shadow mb-1 rounded mt-3 mx-4 px-3 alg-card-hover">
+                    <a href="category.php" class="text-decoration-none">
+                        <div class="d-flex flex-column align-items-center">
+                            <img src="resources/image/car.jpg" alt="" class="alg-category-img mt-4 mb-4 img-fluid">
+                            <span class="mt-1 p-3 fw-bold text-whit pb-5 alg-text-h3">${element.category_type}</span>
+                        </div>
+                </div></a>`
+                });
+              
             } else if (data.status == "failed") {
                 console.log(data.error);
             } else {
