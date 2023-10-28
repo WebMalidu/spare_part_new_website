9aca22bb8cda671c4f7c56aa46abfd044c76fe71
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-10-29 02:25:05 +0530

    testmp

--- backend/api/categoryItemLoad.php
+++ backend/api/categoryItemLoad.php
@@ -33,63 +33,66 @@ $categoryId = $_GET['category_id'];
 $count = $_GET['count'];
 
 
-//get data base model
-$db = new database_driver();
-$searchQueryAll = "SELECT * FROM `category_item` WHERE `category_category_id`=?";
-$resultSets = $db->execute_query($searchQueryAll, 's', array($categoryId));
+echo $categoryId;
+echo $count;
 
-$num = $resultSets['result']->num_rows;
-$perPageCount = 12;
-$pageCount = 0;
+// //get data base model
+// $db = new database_driver();
+// $searchQueryAll = "SELECT * FROM `category_item` WHERE `category_category_id`=?";
+// $resultSets = $db->execute_query($searchQueryAll, 's', array($categoryId));
 
-$pageCount = ceil($num / $perPageCount);
-$responseObject->countPage = $pageCount;
+// $num = $resultSets['result']->num_rows;
+// $perPageCount = 12;
+// $pageCount = 0;
 
-$pageOffset = $perPageCount * $count;
+// $pageCount = ceil($num / $perPageCount);
+// $responseObject->countPage = $pageCount;
 
-$searchQuery = "SELECT * FROM `category_item` WHERE `category_category_id`=? LIMIT " . $perPageCount . " OFFSET " . $pageOffset;
-$resultSet = $db->execute_query($searchQuery, 's', array($categoryId));
+// $pageOffset = $perPageCount * $count;
 
-//response array 
-$responseArray = [];
+// $searchQuery = "SELECT * FROM `category_item` WHERE `category_category_id`=? LIMIT " . $perPageCount . " OFFSET " . $pageOffset;
+// $resultSet = $db->execute_query($searchQuery, 's', array($categoryId));
 
-// image manager
-$directory = "../../resources/image/categoryItemImages";
-$fileExtensions = ['png', 'jpeg', 'jpg', 'svg'];
+// //response array 
+// $responseArray = [];
+
+// // image manager
+// $directory = "../../resources/image/categoryItemImages";
+// $fileExtensions = ['png', 'jpeg', 'jpg', 'svg'];
 
 
-if ($resultSet['result']->num_rows > 0) {
+// if ($resultSet['result']->num_rows > 0) {
 
-     $groupedResults = []; // Create an array to group results
+//      $groupedResults = []; // Create an array to group results
 
-     while ($rowData = $resultSet['result']->fetch_assoc()) {
-          $categoryItemId = $rowData['category_item_id']; // Use categoryName instead of category_type
+//      while ($rowData = $resultSet['result']->fetch_assoc()) {
+//           $categoryItemId = $rowData['category_item_id']; // Use categoryName instead of category_type
 
-          $fileSearch = new FileSearch($directory, $categoryItemId, $fileExtensions); // Use categoryName as the search parameter
+//           $fileSearch = new FileSearch($directory, $categoryItemId, $fileExtensions); // Use categoryName as the search parameter
 
-          $searchResults = $fileSearch->search();
+//           $searchResults = $fileSearch->search();
 
-          $resRowDetailObject = new stdClass();
+//           $resRowDetailObject = new stdClass();
 
-          $resRowDetailObject->category_Item_type = $rowData['category_item_name'];
-          $resRowDetailObject->category_id = $rowData['category_category_id'];
-          $resRowDetailObject->category_Item_id = $categoryItemId; // Use categoryName
+//           $resRowDetailObject->category_Item_type = $rowData['category_item_name'];
+//           $resRowDetailObject->category_id = $rowData['category_category_id'];
+//           $resRowDetailObject->category_Item_id = $categoryItemId; // Use categoryName
 
-          if (is_array($searchResults)) {
-               foreach ($searchResults as $searchResult) {
-                    $resRowDetailObject->category_image = $searchResult;
-               }
-          } else {
-               $responseObject->error = $searchResults;
-               response_sender::sendJson($responseObject);
-          }
+//           if (is_array($searchResults)) {
+//                foreach ($searchResults as $searchResult) {
+//                     $resRowDetailObject->category_image = $searchResult;
+//                }
+//           } else {
+//                $responseObject->error = $searchResults;
+//                response_sender::sendJson($responseObject);
+//           }
 
-          array_push($responseArray, $resRowDetailObject);
-     }
-     $responseObject->status = 'success';
-     $responseObject->results = $responseArray;
-     response_sender::sendJson($responseObject);
-} else {
-     $responseObject->error = 'no row data';
-     response_sender::sendJson($responseObject);
-}
+//           array_push($responseArray, $resRowDetailObject);
+//      }
+//      $responseObject->status = 'success';
+//      $responseObject->results = $responseArray;
+//      response_sender::sendJson($responseObject);
+// } else {
+//      $responseObject->error = 'no row data';
+//      response_sender::sendJson($responseObject);
+// }
