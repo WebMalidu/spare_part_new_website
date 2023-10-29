df4aea3e547ae1798e6fc624824db297aa9ee519
MADUSHADEV <madusha.dev001@gmail.com>
2023-10-29 22:18:10 +0530

    config

--- backend/config.php
+++ backend/config.php
@@ -1,10 +1,5 @@
 <?php
 
-define("DATABASE", "battalk_battalk");
-define("PASSWORD", "3L=1ki$]17_f");
-define("USERNAME", "battalk_madusha");
-
-// define("DATABASE", "alg006_battalk");
-// define("PASSWORD", "#Apeamma2001");
-// define("USERNAME", "root");
-
+define("DATABASE", "alg006_battalk");
+define("PASSWORD", "#Apeamma2001");
+define("USERNAME", "root");
