d28e2b62a834e797c0b78d69a70379446b5e88ca
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2023-12-01 21:27:17 +0530

    multvendo registration page upload

---
+++ multvendoRegister.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multvendo Register</title>
+    <link rel="icon" href="resources/image/home/engineImage.png" />
+
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+    <!-- splide -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.0.9/dist/css/splide.min.css">
+    <!-- css -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@500&family=Lato:wght@300&family=Roboto&family=Rubik&display=swap" rel="stylesheet">
+
+
+    <!-- js -->
+    <script src="https://kit.fontawesome.com/79961d807c.js" crossorigin="anonymous"></script>
+    <script defer src="js/bootstrap.bundle.js"></script>
+    <script defer src="js/script.js"></script>
+    <script defer src="js/slider.js"></script>
+    <script defer src="js/vehicleFetchData.js"></script>
+    <script defer src="js/home.js"></script>
+    <script defer src="js/product.js"></script>
+
+    <script defer src="js/toast.js"></script>
+
+    <script defer src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+
+
+</head>
+
+<body class="w-100 mrp-body alg-bg-light-blue">
+    <!-- header section-->
+    <?php include("./pages/components/header.php") ?>
+
+
+    <!-- content -->
+    <div class="mrp-mainbox d-flex  justify-content-center align-items-center">
+        <div class="mrp-box alg-bg-light d-flex flex-column align-items-center alg-rounded-small">
+            <div class="d-flex justify-content-center pt-4">
+                <span class="alg-text-h2 alg-bold alg-text-dark-blue">Multvendo Registration</span>
+            </div>
+            <div class="d-flex flex-column pt-5">
+                <span class="alg-text-h3 alg-text-blue">Business Owner Name</span>
+                <div class="mrp-input-box pt-1">
+                    <input class="mrp-input  alg-rounded-small alg-shadow" type="text">
+                </div>
+            </div>
+            <div class="d-flex flex-column pt-4">
+                <span class="alg-text-h3 alg-text-blue">Address</span>
+                <div class="mrp-input-box pt-1">
+                    <input class="mrp-input  alg-rounded-small alg-shadow" type="text">
+                </div>
+            </div>
+            <div class="d-flex flex-column pt-4">
+                <span class="alg-text-h3 alg-text-blue">Business Name</span>
+                <div class="mrp-input-box pt-1">
+                    <input class="mrp-input  alg-rounded-small alg-shadow" type="text">
+                </div>
+            </div>
+            <div class="d-flex flex-column pt-4">
+                <span class="alg-text-h3 alg-text-blue">BR Number</span>
+                <div class="mrp-input-box pt-1">
+                    <input class="mrp-input  alg-rounded-small alg-shadow" type="text">
+                </div>
+            </div>
+            <div class="d-flex flex-column pt-4">
+                <span class="alg-text-h3 alg-text-blue">Business Address</span>
+                <div class="mrp-input-box pt-1">
+                    <input class="mrp-input  alg-rounded-small alg-shadow" type="text">
+                </div>
+            </div>
+            <div class="pt-4">
+                <span class="alg-text-h3 alg-text-blue ">Contact Number</span>
+                <div class="mrp-contact-box d-flex">
+                    <div>
+                        <span class="alg-text-h3 alg-text-blue">Business</span>
+                        <div class="mrp-input-box pt-1">
+                            <input class="mrp-input-small  alg-rounded-small alg-shadow" type="text">
+                        </div>
+                    </div>
+                    <div>
+                        <span class="alg-text-h3 alg-text-blue">Private</span>
+                        <div class="mrp-input-box pt-1">
+                            <input class="mrp-input-small  alg-rounded-small alg-shadow" type="text">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-column pt-4">
+                <span class="alg-text-h3 alg-text-blue">Email Address</span>
+                <div class="mrp-input-box pt-1">
+                    <input class="mrp-input  alg-rounded-small alg-shadow" type="text">
+                </div>
+            </div>
+            <div class="p-4">
+                <button class="mrp-button alg-rounded-small alg-text-blue alg-text-h3">Register</button>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- footer -->
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.0.9/dist/js/splide.min.js"></script>
+</body>
