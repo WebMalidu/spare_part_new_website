d02bbd2c7e4a4f70b97cc4f9040854bd2b654682
MADUSHADEV <madusha.dev001@gmail.com>
2023-12-03 14:27:53 +0530

    product Adding UI upadate

--- admin/components/mainNavigationPanels/productPanel.php
+++ admin/components/mainNavigationPanels/productPanel.php
@@ -13,5 +13,48 @@
               <div class="p-2 h-100 d-none alg-bg-light alg-rounded-small overflow-auto flex-grow-1" id="productViewSection">
 
               </div>
+              <div class="p-2  h-100 d-none alg-bg-light alg-rounded-small overflow-auto flex-grow-1" id="productAddSection">
+                     <div class="w-100">
+                            <label class="form-label" for="productTitleInputField">Product Title</label>
+                            <input type="text" id="productTitleInputField" class="form-control">
+                     </div>
+                     <div class="w-100">
+                            <label class="form-label" for="productOrigin">Product Origin</label>
+                            <select id="productOrigin" class="form-select">
+                                   Something Went Wrong..!!
+                            </select>
+                     </div>
+                     <div class="w-100">
+                            <label class="form-label" for="productQty">Product QTY</label>
+                            <input type="number" id="productQty" class="form-control">
+                     </div>
+                     <div class="w-100">
+                            <label class="form-label" for="productDescriptionInputField">Product Description</label>
+                            <textarea type="text" id="productDescriptionInputField" class="form-control" rows="5"></textarea>
+                     </div>
+                     <div class="w-100">
+                            <label class="form-label" for="productPrice">Product Price</label>
+                            <input type="number" id="productPrice" class="form-control"></input>
+                     </div>
+                     <div class="w-100">
+                            <label class="form-label" for="productBrand">Brand</label>
+                            <select id="productBrand" class="form-select">
+                                   Something Went Wrong..!!
+                            </select>
+                     </div>
+                     <div class="w-100">
+                            <label class="form-label" for="productModelLine">Model Line</label>
+                            <select id="productModelLine" class="form-select">
+                                   Something Went Wrong..!!
+                            </select>
+                     </div>
+                     <div class="w-100">
+                            <label class="form-label" for="productShippingPrice">Shipping Price</label>
+                            <input type="number" id="productShippingPrice" class="form-control"></input>
+                     </div>
+                     <div class="w-100">
+                            <button onclick="addProduct()" class="my-4 w-100 alg-btn-pill">Add Product</button>
+                     </div>
+              </div>
        </div>
 </section>
